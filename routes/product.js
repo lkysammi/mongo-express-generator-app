0c45c0ab1331206e52dc9d98a9c57620e3f7d376
lkysammi <kleun136@my.centennialcollege.ca>
2023-06-07 21:51:52 -0400

    crud operation

---
+++ routes/product.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//connect to model
+let Product = require('../models/product');
+
+//Manage routes
+router.get('/', (req, res, next) => {
+    Product.find((err, productList) => {
+        if(err){
+            return console.error(err);
+        }else{
+            //console.log(productList);
+            res.render('product/list', {title: 'Product Info', ProductList: productList})
+        }
+    });
+});
+
+//to open add product page
+router.get('/add', (req, res, next) => {
+    res.render('product/add', {title: 'Add Product'})
+});
+
+//insert product data into mongoDB collection
+router.post('/add', (req, res, next) => {
+    //getting data from form
+    let newProduct = Product({
+        "name": req.body.pname,
+        "company": req.body.pcompany,
+        "price": req.body.price
+    });
+
+    //insert data into the mongoDB
+    Product.create(newProduct, (err, Product) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }else{
+            res.redirect('/products')
+        }
+    });
+});
+
+module.exports = router;
