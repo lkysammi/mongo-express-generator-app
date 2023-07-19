25dc5af763967c0f5b2c647f2cc6cc288cc963aa
lkysammi <kleun136@my.centennialcollege.ca>
2023-07-18 20:54:26 -0400

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IBM' });
-});
-
-/* GET home page. */
-router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Home' });
 });
 
